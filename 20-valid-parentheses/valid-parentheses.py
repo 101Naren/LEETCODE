61e61f4569907091bc25ca5af84eb70b47f3398f
Naren <01naren.push@gmail.com>
2026-03-11 22:38:50 +0530

    Time: 1 ms (86.85%) | Memory: 12.6 MB (36.11%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        for ch in s:
+            if ch in "({[":
+                stack.append(ch)
+            else:
+                if not stack or stack.pop() != mapping[ch]:
+                    return False
+
+        return not stack
+
